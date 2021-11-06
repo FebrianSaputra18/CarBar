c68e152af211b9f96ff9c031d926cf7330c110b6
Klarionic <hongwilaheng18@gmail.com>
2021-11-06 17:54:10 +0700

    first

---
+++ resources/views/index.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Advent+Pro:wght@500&display=swap" rel="stylesheet">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <link rel="stylesheet" href="style.css">
+
+    <title>Cari Barengan</title>
+</head>
+
+<body>
+
+    <div class="intro">
+        <h1 class="logo-header">
+            <span class="logo">Car</span><span class="logo">Bar</span>
+        </h1>
+    </div>
+
+    <header>
+        <h4>CarBar.</h4>
+    </header>
+    <script src="{{asset('app.js')}}"></script>
+</body>
+
+
+</html>
